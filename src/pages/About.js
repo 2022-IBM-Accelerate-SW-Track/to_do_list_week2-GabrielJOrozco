678bea15a40c752cacce7418bb10051f1050b03b
GabrielJOrozco <gabrieljorozco2002@gmail.com>
2022-06-17 20:50:34 -0400

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,38 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
 import profile_pic from "../assets/profile_pic.jpg";
+
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            src={profile_pic}
+            alt="Profile Pic"
             ></img>
-          </div>
         </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
+      </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Gabriel Orozco</div>
+          <div className="brief_description">
+            <p>I am a rising Sophomore at Vanderbilt University studying Computer Science! Here's a little bit more about me:</p>
+            <ul>
+              <p>
+                I am on Vanderbilt's Varsity Table Tennis Team
+              </p>
+              <p>
+                I also have interests in Business, Psychology, and Exercise Science
+              </p>
+            </ul>
           </div>
         </div>
       </div>
-    );
+    </div>
+    )
   }
 }
